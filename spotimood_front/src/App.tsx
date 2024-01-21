9a3d1b478d4460b7e870c35d0a3c1184cc6dfb64
Moh T <tmd_larbi@hotmail.com>
2024-01-21 10:36:57 -0500

    commit after destruction

--- spotimood_front/src/App.tsx
+++ spotimood_front/src/App.tsx
@@ -1,10 +1,11 @@
-import Navigation from './components/Navigation';
-import Loading from './components/Loading';
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
 //import bgVideo from './assets/backgroundvideo.mp4';
-import Spline from '@splinetool/react-spline';
-import { Spotify } from 'react-spotify-embed';
+import Spline from "@splinetool/react-spline";
+import { Spotify } from "react-spotify-embed";
 import axios from "axios";
 import { useState } from "react";
+import Slider from "@mui/material/Slider";
 
 interface Playlist {
   songs: string[];
@@ -20,13 +21,14 @@ export default function App() {
   };
   const [link, setLink] = useState("");
 
-  async function generatePlaylist(mood: string) {
+  async function generatePlaylist(mood: string, songs: string) {
     setIsLoading(true);
     await axios
       .post(
         "http://172.30.182.167:8080/generate",
         {
           mood: mood,
+          nb_songs: songs,
         },
         { timeout: 30000 }
       )
@@ -53,44 +55,87 @@ export default function App() {
     setIsLoading(false);
     setReceived(true);
   }
+  const marks = [
+    {
+      value: 10,
+      label: '10'
+    },
+    {
+      value: 30,
+      label: '30',
+    },
+  ];
   return (
     <>
-    <Loading />
-    <Navigation />
-      <div className='scene'> 
+      <Loading />
+      <Navigation />
+      <div className="scene">
         {/*<video src={bgVideo} autoPlay loop muted/>*/}
-        <div className='interactive'>
-        {link ? <Spotify className='spotify_player' link={link} /> : <></>}
-        <div className='home_text'>
-        <form
-      className="card"
-      onSubmit={(e) => {
-        e.preventDefault();
-        generatePlaylist(e.target[0].value);
-        console.log(e.target[0].value);
-        e.target[0].value = "";
-      }}
-    >
-      {isLoading && !received ? (
-        <div className="loading_gpt">
-            Your playlist is being generated...
-        </div>
-      ) : !isLoading && !received ? (
-        <>
-          <h2>How is your mood today?</h2>
-          <label className="input">
-            <input className="input__field" type="text" placeholder=" " />
-          </label>
-          <div className="button-group">
-            <button>Send</button>
+        <div className="interactive">
+          {link ? <Spotify className="spotify_player" link={link} /> : <></>}
+          <div className="home_text">
+            <form
+              className="card"
+              onSubmit={(e) => {
+                e.preventDefault();
+                generatePlaylist(e.target[0].value, e.target[1].value);
+                console.log(e.target[0].value, e.target[1].value);
+                e.target[1].value = "";
+              }}
+            >
+              {isLoading && !received ? (
+                <div className="loading_gpt">
+                  Your playlist is being generated...
+                </div>
+              ) : !isLoading && !received ? (
+                <>
+                  <h2>How is your mood today?</h2>
+
+                  <label className="input">
+                    <input
+                      className="input__field"
+                      type="text"
+                      placeholder=" "
+                    />
+                  </label>
+                  <div className="slider-title">Number of songs in your playlist</div>
+                  <Slider
+                    aria-label="Always visible"
+                    defaultValue={10}
+                    min={10}
+                    max={30}
+                    step={1}
+                    marks={marks}
+                    valueLabelDisplay="auto"
+                    style={{ width: "80%", color: "var(--primary-color)", margin: "5px"}}
+                  />
+                  <div className="button-group">
+                    <button>Send</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <a className="playlist" href={link} target="_blank">
+                    Open in Spotify
+                  </a>
+                  <div
+                    className="generate_playlist"
+                    onClick={() => {
+                      setReceived(false);
+                      setLink("");
+                    }}
+                  >
+                    Generate another playlist
+                  </div>
+                </>
+              )}
+            </form>
           </div>
-        </>
-      ) : (
-          <><a className="playlist" href={link} target='_blank'>Open in Spotify</a><div className="generate_playlist" onClick={() => {setReceived(false);setLink("")}}>Generate another playlist</div></>
+          {link ? (
+            <div className="prop"></div>
+          ) : (
+            <Spline scene="https://prod.spline.design/YoQByJBAq8JIoq4S/scene.splinecode" />
           )}
-    </form>
-        </div>
-        {link ? <div className='prop'></div> : <Spline scene="https://prod.spline.design/YoQByJBAq8JIoq4S/scene.splinecode" />}
         </div>
       </div>
     </>
